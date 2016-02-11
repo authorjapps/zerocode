607b8d313e2d244c9fa038b4b50952aa340e5578
Nirmal Chandra <nirmal.fleet@gmail.com>
2016-02-11 17:34:56 +0000

    OS-BUILD-TEST - Added space to sout message

--- src/main/java/org/jsmart/online/hello/HelloFirst.java
+++ src/main/java/org/jsmart/online/hello/HelloFirst.java
@@ -3,7 +3,7 @@ package org.jsmart.online.hello;
 public class HelloFirst {
 
     public int add(int i, int i1) {
-        System.out.println("i=" + i + ", j=" + i1);
+        System.out.println("i= " + i + ", j= " + i1);
         return i + i1;
     }
 }
