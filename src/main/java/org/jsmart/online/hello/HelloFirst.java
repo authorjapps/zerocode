2c279dd268a3ead481a6541a2ee42348faac6984
Nirmal Chandra <nirmal.fleet@gmail.com>
2016-02-11 16:56:33 +0000

    OS-BUILD-TEST - Green build check - Configure online

---
+++ src/main/java/org/jsmart/online/hello/HelloFirst.java
@@ -0,0 +1,9 @@
+package org.jsmart.online.hello;
+
+public class HelloFirst {
+
+    public int add(int i, int i1) {
+        System.out.println("i=" + i + ", j=" + i1);
+        return i + i1;
+    }
+}
---
+++ src/test/java/org/jsmart/online/hello/HelloFirstTest.java
@@ -0,0 +1,17 @@
+package org.jsmart.online.hello;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class HelloFirstTest {
+
+    @Test
+    public void willAddTwoWholeNumbers() throws Exception {
+        HelloFirst helloFirst = new HelloFirst();
+        int sum = helloFirst.add(4, 6);
+
+        assertThat("Did not add properly", sum, is(10));
+    }
+}
