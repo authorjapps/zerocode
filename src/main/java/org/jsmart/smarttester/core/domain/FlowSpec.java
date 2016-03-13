9bde6cea9a16ccd54e95d963e493c9f80219bb45
Nirmal Chandra <nirmal.fleet@gmail.com>
2016-03-13 12:35:41 +0000

    OS-SMART # Flow Deserialize test

--- src/main/java/org/jsmart/smarttester/core/domain/FlowSpec.java
+++ src/main/java/org/jsmart/smarttester/core/domain/FlowSpec.java
@@ -1,24 +1,46 @@
 package org.jsmart.smarttester.core.domain;
 
-import java.util.List;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 public class FlowSpec {
 
-    private final String flow;
+    private final Integer loop;
+    private final String flowName;
     private List<Step> steps; // = new ArrayList<Step>();
 
-    public FlowSpec(String flow, List<Step> steps) {
-        this.flow = flow;
+    @JsonCreator
+    public FlowSpec(
+            @JsonProperty("loop") Integer loop,
+            @JsonProperty("flowName") String flowName,
+            @JsonProperty("steps") List<Step> steps) {
+        this.loop = loop;
+        this.flowName = flowName;
         this.steps = steps;
     }
 
-    public String getFlow() {
-        return flow;
+    public String getFlowName() {
+        return flowName;
     }
 
     public List<Step> getSteps() {
-        return steps;
-        //Optional.ofNullable(steps)
-        //.orElse(Collections.<Step>emptyList());
+        // return steps;
+        return Optional.ofNullable(steps)
+                .orElse(Collections.<Step>emptyList());
+    }
+
+    @Override
+    public String toString() {
+        return "FlowSpec{" +
+                "loop=" + loop +
+                ", flowName='" + flowName + '\'' +
+                ", steps=" + steps +
+                '}';
     }
 }
---
+++ src/test/java/org/jsmart/smarttester/core/domain/FlowSpecTest.java
@@ -0,0 +1,56 @@
+package org.jsmart.smarttester.core.domain;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsmart.smarttester.core.utils.SmartUtils;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.fail;
+
+public class FlowSpecTest {
+    private ObjectMapper mapper;
+
+    @Before
+    public void beforeMethod() throws Exception {
+        mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
+    }
+
+    @Test
+    public void willDeserializeA_VanilaFlow() throws Exception {
+        String jsonDocumentAsString = SmartUtils.getJsonDocumentAsString("smart_test_cases/02_test_json_flow_single_step.json", this);
+        FlowSpec flowDeserialized = mapper.readValue(jsonDocumentAsString, FlowSpec.class);
+
+        assertThat(flowDeserialized, notNullValue());
+        assertThat(flowDeserialized.getSteps().size(), is(1));
+        assertThat(flowDeserialized.getFlowName(), containsString("Given_When_Then-Flow"));
+    }
+
+    @Test
+    public void willDeserializeA_MultiSteps() throws Exception {
+        String jsonDocumentAsString = SmartUtils.getJsonDocumentAsString("smart_test_cases/03_test_json_flow_multi_step.json", this);
+        FlowSpec flowDeserialized = mapper.readValue(jsonDocumentAsString, FlowSpec.class);
+
+        assertThat(flowDeserialized, notNullValue());
+        assertThat(flowDeserialized.getSteps().size(), is(2));
+        assertThat(flowDeserialized.getFlowName(), containsString("Given_When_Then-Flow"));
+    }
+
+    @Test
+    @Ignore
+    public void willComplainForDuplicateNames_Step() throws Exception {
+        fail();
+    }
+
+    @Test
+    @Ignore
+    public void willComplainForDuplicateNames_Flow() throws Exception {
+        fail();
+    }
+}
