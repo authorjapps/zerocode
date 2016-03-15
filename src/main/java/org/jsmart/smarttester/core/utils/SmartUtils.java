1870804b1d437b4edb08e9fbaf9ebf1a4a1e92f0
Nirmal Chandra <nirmal.fleet@gmail.com>
2016-03-15 15:57:03 +0000

    OS-SMART #Read all json docs from resource package

--- src/main/java/org/jsmart/smarttester/core/utils/SmartUtils.java
+++ src/main/java/org/jsmart/smarttester/core/utils/SmartUtils.java
@@ -1,14 +1,30 @@
 package org.jsmart.smarttester.core.utils;
 
+import com.google.classpath.ClassPath;
+import com.google.classpath.ClassPathFactory;
+import com.google.classpath.RegExpResourceFilter;
 import com.google.common.io.Resources;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 public class SmartUtils {
 
     public static <T> String getJsonDocumentAsString(String name, T clazz) throws IOException {
         String jsonAsString = Resources.toString(clazz.getClass().getClassLoader().getResource(name), StandardCharsets.UTF_8);
         return jsonAsString;
+    }
+
+    public static List<String> getAllEndPointFiles(String packageName) {
+        ClassPathFactory factory = new ClassPathFactory();
+        ClassPath jvmClassPath = factory.createFromJVM();
+        String[] allSimulationFiles = jvmClassPath.findResources(packageName, new RegExpResourceFilter(".*", ".*\\.json$"));
+        if(null == allSimulationFiles || allSimulationFiles.length == 0) {
+            throw new RuntimeException("YouTriedToSimulateNothingException: Check the (" + packageName + ") integration test repo folder(empty?). " );
+        }
+
+        return Arrays.asList(allSimulationFiles);
     }
 }
--- src/test/java/org/jsmart/smarttester/core/utils/SmartUtilsTest.java
+++ src/test/java/org/jsmart/smarttester/core/utils/SmartUtilsTest.java
@@ -1,17 +1,18 @@
 package org.jsmart.smarttester.core.utils;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.fail;
 
 public class SmartUtilsTest {
 
 
     @Test
-    public void willGetJsonFileIntoAJavaString() throws  Exception{
+    public void willGetJsonFileIntoA_JavaString() throws  Exception{
         String jsonDocumentAsString = SmartUtils.getJsonDocumentAsString("smart_test_cases/01_test_json_single_step.json", this);
         assertThat(jsonDocumentAsString, containsString("assertions"));
         assertThat(jsonDocumentAsString, containsString("request"));
@@ -20,10 +21,9 @@ public class SmartUtilsTest {
     }
 
     @Test
-    @Ignore
-    public void willReadAllfilesFromTestResource() throws Exception {
-
-        fail();
-
+    public void willReadAllfileNamesFrom_TestResource() throws Exception {
+        List<String> allTestCaseFiles = SmartUtils.getAllEndPointFiles("smart_test_cases");
+        assertThat(allTestCaseFiles.size(), is(4));
+        assertThat(allTestCaseFiles.get(0).toString(), is("smart_test_cases/01_test_json_single_step.json"));
     }
 }
