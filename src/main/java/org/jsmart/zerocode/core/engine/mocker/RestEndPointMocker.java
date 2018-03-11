e67fe2cf793686dfa8ec28fee9fe453ef39fd15f
Author jSmart <author.japps@gmail.com>
2018-03-11 13:08:44 +0000

    ISSUE-47 # WireMock - Optional dynamic mock.api.port for downstream services

--- src/main/java/org/jsmart/zerocode/core/engine/mocker/RestEndPointMocker.java
+++ src/main/java/org/jsmart/zerocode/core/engine/mocker/RestEndPointMocker.java
@@ -1,50 +1,118 @@
 package org.jsmart.zerocode.core.engine.mocker;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.apache.commons.lang.StringUtils;
+import org.jsmart.zerocode.core.domain.MockStep;
 import org.jsmart.zerocode.core.domain.MockSteps;
+import org.jsmart.zerocode.core.engine.executor.JsonServiceExecutorImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.equalToJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.givenThat;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.put;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class RestEndPointMocker {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonServiceExecutorImpl.class);
 
-    public static void createWithWireMock(MockSteps mockSteps) {
-        WireMockServer wireMockServer = new WireMockServer(wireMockConfig().port(8888)); //<-- Strange
-        wireMockServer.start();
-        WireMock.configureFor("localhost", 8888); //<-- Repetition of PORT
+    public static WireMockServer wireMockServer;
 
-        // TODO - Check wiremock APIs to whether it supports method as a paramater or direct JSON
+    public static void createWithWireMock(MockSteps mockSteps, int mockPort) {
+
+        restartWireMock(mockPort);
+
         mockSteps.getMocks().forEach(mockStep -> {
             String jsonBodyRequest = mockStep.getResponse().get("body").toString();
 
             if("GET".equals(mockStep.getOperation())){
-                givenThat(WireMock.get(urlEqualTo(mockStep.getUrl()))
-                        .willReturn(aResponse()
-                                .withStatus(mockStep.getResponse().get("status").asInt())
-                                .withHeader("Content-Type", APPLICATION_JSON)
-                                .withBody(jsonBodyRequest)));
+                LOGGER.info("*****WireMock- Mocking the GET endpoint");
+                givenThat(createGetRequestBuilder(mockStep)
+                        .willReturn(responseBuilder(mockStep, jsonBodyRequest)));
+                LOGGER.info("WireMock- Mocking the GET endpoint -done- *****");
             }
             else if("POST".equals(mockStep.getOperation())){
-                givenThat(WireMock.post(urlEqualTo(mockStep.getUrl()))
-                        .willReturn(aResponse()
-                                .withStatus(mockStep.getResponse().get("status").asInt())
-                                .withHeader("Content-Type", APPLICATION_JSON)
-                                .withBody(jsonBodyRequest)));
+                LOGGER.info("*****WireMock- Mocking the POST endpoint");
+                givenThat(createPostRequestBuilder(mockStep)
+                        .willReturn(responseBuilder(mockStep, jsonBodyRequest)));
+                LOGGER.info("WireMock- Mocking the POST endpoint -done-*****");
             }
             else if("PUT".equals(mockStep.getOperation())){
-                givenThat(WireMock.put(urlEqualTo(mockStep.getUrl()))
-                        .willReturn(aResponse()
-                                .withStatus(mockStep.getResponse().get("status").asInt())
-                                .withHeader("Content-Type", APPLICATION_JSON)
-                                .withBody(jsonBodyRequest)));
+                LOGGER.info("*****WireMock- Mocking the PUT endpoint");
+                givenThat(createPutRequestBuilder(mockStep)
+                        .willReturn(responseBuilder(mockStep, jsonBodyRequest)));
+                LOGGER.info("WireMock- Mocking the PUT endpoint -done-*****");
             }
         });
     }
+
+
+    public static void restartWireMock(int dynamicPort) {
+        wireMockServer = new WireMockServer(wireMockConfig().port(dynamicPort)); // <-- Strange
+        /*
+         * Stop the wireMock server if it is running previously due to any other tests.
+         */
+        wireMockServer.stop();
+        wireMockServer.start();
+        WireMock.configureFor("localhost", dynamicPort); // <-- Repetition of PORT was needed, this is a wireMock bug
+    }
+
+    private static MappingBuilder createPutRequestBuilder(MockStep mockStep) {
+        final MappingBuilder requestBuilder = put(urlEqualTo(mockStep.getUrl()));
+        return createRequestBuilderWithHeaders(mockStep, requestBuilder);
+    }
+
+    private static MappingBuilder createPostRequestBuilder(MockStep mockStep) {
+        final MappingBuilder requestBuilder = post(urlEqualTo(mockStep.getUrl()));
+        return createRequestBuilderWithHeaders(mockStep, requestBuilder);
+    }
+
+    private static MappingBuilder createGetRequestBuilder(MockStep mockStep) {
+        final MappingBuilder requestBuilder = get(urlEqualTo(mockStep.getUrl()));
+        return createRequestBuilderWithHeaders(mockStep, requestBuilder);
+    }
+
+    private static MappingBuilder createRequestBuilderWithHeaders(MockStep mockStep, MappingBuilder requestBuilder) {
+
+        final String bodyJson = mockStep.getBody();
+        // -----------------------------------------------
+        // read request body and set to request builder
+        // -----------------------------------------------
+        if(StringUtils.isNotEmpty(bodyJson)){
+            requestBuilder.withRequestBody(equalToJson(bodyJson));
+        }
+
+        final Map<String, Object> headersMap = mockStep.getHeadersMap();
+        // -----------------------------------------------
+        // read request headers and set to request builder
+        // -----------------------------------------------
+        if (headersMap.size() > 0) {
+            for (Object key : headersMap.keySet()) {
+                requestBuilder.withHeader((String) key, equalTo((String) headersMap.get(key)));
+            }
+        }
+        return requestBuilder;
+    }
+
+    private static ResponseDefinitionBuilder responseBuilder(MockStep mockStep, String jsonBodyRequest) {
+        final ResponseDefinitionBuilder responseBuilder = aResponse()
+                .withStatus(mockStep.getResponse().get("status").asInt())
+                //.withHeader("Content-Type", APPLICATION_JSON) //Can be XMl also for SOAP end points
+                .withBody(jsonBodyRequest);
+
+        return responseBuilder;
+    }
+
 
     /*
      * This is working code, whenever you put the virtuoso dependency here, you can uncomment this block.
