df3cbd3f044ee5f7f85aa4f7614aa644547c86c6
Santhoshkumar S <santhosh.sk@zohocorp.com>
2018-12-26 13:54:05 +0530

    Enabled Kafka related tests for CI

--- src/test/java/org/jsmart/zerocode/kafka/customclient/KafkaProduceCustomClientTest.java
+++ src/test/java/org/jsmart/zerocode/kafka/customclient/KafkaProduceCustomClientTest.java
@@ -5,11 +5,9 @@ import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.domain.UseKafkaClient;
 import org.jsmart.zerocode.core.kafka.client.ZerocodeCustomKafkaClient;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-@Ignore("Ignored for CI build until docker ci build setup is complete. But can be run manually against a kafka server.")
 @TargetEnv("hosts_servers/kafka_test_server.properties")
 @UseKafkaClient(ZerocodeCustomKafkaClient.class)
 @RunWith(ZeroCodeUnitRunner.class)
