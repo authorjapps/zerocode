a84ac63b3fec12d599356aae4303741306ab3708
Nirmal Chandra <nirmal.fleet@gmail.com>
2019-01-10 04:52:38 +0000

    ISS-182 # [REVIEW-COMMENT] Deleted redundant classes, variables

--- src/main/java/org/jsmart/zerocode/core/kafka/KafkaConstants.java
+++ src/main/java/org/jsmart/zerocode/core/kafka/KafkaConstants.java
@@ -1,25 +1,10 @@
 package org.jsmart.zerocode.core.kafka;
 
 public interface KafkaConstants {
-    String KAFKA_BROKERS = "192.168.99.1:9092";
-
-    Integer MESSAGE_COUNT = 110;
-
-    String CLIENT_ID = "client1";
-
-    String TOPIC_NAME = "demo";
-
-    String GROUP_ID_CONFIG = "consumerGroup10";
 
     Integer MAX_NO_OF_RETRY_POLLS_OR_TIME_OUTS = 5;
 
     Long DEFAULT_POLLING_TIME_MILLI_SEC = 100L;
-
-    String OFFSET_RESET_LATEST = "latest";
-
-    String OFFSET_RESET_EARLIER = "earliest";
-
-    Integer MAX_POLL_RECORDS = 1;
 
     String RAW = "RAW";
 
