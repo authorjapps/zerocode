8573d183c49a1453b5352e400b2ee682c0e93586
Author JApps <author.japps@gmail.com>
2019-01-13 10:18:44 +0000

    ISS-187 # KSQL basics -Green

---
+++ src/test/java/org/jsmart/zerocode/kafka/consume/ksql/KafkaKsqlTest.java
@@ -0,0 +1,18 @@
+package org.jsmart.zerocode.kafka.consume.ksql;
+
+import org.jsmart.zerocode.core.domain.JsonTestCase;
+import org.jsmart.zerocode.core.domain.TargetEnv;
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@TargetEnv("kafka_servers/kafka_test_server_avro.properties")
+@RunWith(ZeroCodeUnitRunner.class)
+public class KafkaKsqlTest {
+
+    @Test
+    @JsonTestCase("kafka/consume/ksql/test_ksql_query.json")
+    public void testKafkaConsume_ksql() throws Exception {
+    }
+
+}
