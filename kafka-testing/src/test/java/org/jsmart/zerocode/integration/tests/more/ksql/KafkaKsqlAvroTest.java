cc28a6de251691f582e52dbc2cf9a25f9f364cae
Nirmal Chandra <nirmal.fleet@gmail.com>
2019-01-23 07:39:14 +0000

    ISS-189 # KSQL error reproduced -avro

---
+++ kafka-testing/src/test/java/org/jsmart/zerocode/integration/tests/more/ksql/KafkaKsqlAvroTest.java
@@ -0,0 +1,25 @@
+package org.jsmart.zerocode.integration.tests.more.ksql;
+
+import org.jsmart.zerocode.core.domain.JsonTestCase;
+import org.jsmart.zerocode.core.domain.TargetEnv;
+import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@TargetEnv("kafka_servers/kafka_test_server_avro.properties")
+@RunWith(ZeroCodeUnitRunner.class)
+public class KafkaKsqlAvroTest {
+
+    //@Ignore ("Issue still exists")
+    @Test
+    @JsonTestCase("kafka/consume/ksql/WIP_ISSUE_test_ksql_print_records.json")
+    public void testKafkaConsume_printTopicRaw() throws Exception {
+    }
+
+    //@Ignore ("Issue still exists")
+    @Test
+    @JsonTestCase("kafka/consume/ksql/WIP_ISSUE_test_ksql_print_records_json.json")
+    public void testKafkaConsume_printTopicJson() throws Exception {
+    }
+
+}
