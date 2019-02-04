9ee6b6540c09a5da07b3ddff04edef2a48d04baf
Nirmal Chandra <nirmal.fleet@gmail.com>
2019-02-04 21:22:51 +0000

    ISS-199 # [On User Demand] POM - Removed Confluent Avro dependency - Can be added manually to make the tests Pass

--- kafka-testing/src/test/java/org/jsmart/zerocode/integration/tests/kafka/consume/KafkaConsumeAvroTest.java
+++ kafka-testing/src/test/java/org/jsmart/zerocode/integration/tests/kafka/consume/KafkaConsumeAvroTest.java
@@ -3,9 +3,12 @@ package org.jsmart.zerocode.integration.tests.kafka.consume;
 import org.jsmart.zerocode.core.domain.JsonTestCase;
 import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+@Ignore("Users Requested to ignore this until io.confluent:kafka-avro-serializer:5.1.0 becomes available at maven central." +
+        "But to see these tests Passing - Visit repo >> https://github.com/authorjapps/hello-kafka-stream-testing")
 @TargetEnv("kafka_servers/kafka_test_server_avro.properties")
 @RunWith(ZeroCodeUnitRunner.class)
 public class KafkaConsumeAvroTest {
