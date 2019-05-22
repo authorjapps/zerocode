51a51bd1f4cae353757f950d97d3ed798ac14ca6
James Neate <JamesMNeate@gmail.com>
2019-05-22 17:12:33 +0100

    ISS-241 # Display assumed payload and assertions on step error

--- core/src/main/java/org/jsmart/zerocode/core/runner/ZeroCodeMultiStepsScenarioRunnerImpl.java
+++ core/src/main/java/org/jsmart/zerocode/core/runner/ZeroCodeMultiStepsScenarioRunnerImpl.java
@@ -242,6 +242,11 @@ public class ZeroCodeMultiStepsScenarioRunnerImpl implements ZeroCodeMultiStepsS
 
                         if (!failureResults.isEmpty()) {
                             StringBuilder builder = new StringBuilder();
+                            
+                            // JNEATE 21-05-2019 - Print expected Payload along with assertion errors
+                            builder.append("Assumed Payload: \n" + prettyPrintJson(resolvedAssertionJson) + "\n");
+                            builder.append("Assertion Errors: \n");
+                            
                             failureResults.forEach(f -> {
                                 builder.append(f.toString() + "\n");
                             });
