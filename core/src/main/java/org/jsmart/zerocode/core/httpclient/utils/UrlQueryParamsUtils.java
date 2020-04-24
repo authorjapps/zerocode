980cb7e0cc1044759a15f6bc61ab3e068b0e553f
kabarret <kaue.pbarreto@gmail.com>
2020-04-24 16:43:08 -0300

    Issue-383 Make possible to add params on URI and by queryParams simultaneously for Http Requests

--- core/src/main/java/org/jsmart/zerocode/core/httpclient/utils/UrlQueryParamsUtils.java
+++ core/src/main/java/org/jsmart/zerocode/core/httpclient/utils/UrlQueryParamsUtils.java
@@ -1,17 +1,11 @@
 package org.jsmart.zerocode.core.httpclient.utils;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.ArrayList;
+import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static java.util.Optional.ofNullable;
@@ -19,25 +13,14 @@ import static java.util.Optional.ofNullable;
 public class UrlQueryParamsUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(UrlQueryParamsUtils.class);
 
-    public static String setQueryParams(String httpUrl, Map<String, Object> queryParams) throws IOException {
-        String qualifiedQueryParams = createQualifiedQueryParams(queryParams);
-        httpUrl = httpUrl + "?" + qualifiedQueryParams;
-
-        LOGGER.info("### Effective url is : " + httpUrl);
-        return httpUrl;
+    public static String setQueryParams(final String httpUrl, final Map<String, Object> queryParams) throws URISyntaxException {
+        URIBuilder uriBuilder = new URIBuilder(httpUrl);
+        Map<String, Object> nullSafeQueryParams = ofNullable(queryParams).orElseGet(HashMap::new);
+        nullSafeQueryParams.keySet().forEach(key ->
+                uriBuilder.addParameter(key, nullSafeQueryParams.get(key).toString())
+        );
+        String composedURL = uriBuilder.build().toString();
+        LOGGER.info("### Effective url is : {}", composedURL);
+        return composedURL;
     }
-
-    protected static String createQualifiedQueryParams(Map<String, Object> queryParamsMap) throws IOException {
-        queryParamsMap = ofNullable(queryParamsMap).orElse(new HashMap<>());
-        List<NameValuePair> nameValueList = new ArrayList<>();
-        for(String key : queryParamsMap.keySet()) {
-            nameValueList.add(new BasicNameValuePair(key, queryParamsMap.get(key).toString()));
-        }
-        HttpEntity httpEntity = new UrlEncodedFormEntity(nameValueList);
-        String qualifiedQueryParam = EntityUtils.toString(httpEntity, "UTF-8");
-
-        LOGGER.info("### qualifiedQueryParams : " + qualifiedQueryParam);
-        return qualifiedQueryParam;
-    }
-
 }
--- core/src/test/java/org/jsmart/zerocode/core/httpclient/utils/UrlQueryParamsUtilsTest.java
+++ core/src/test/java/org/jsmart/zerocode/core/httpclient/utils/UrlQueryParamsUtilsTest.java
@@ -2,7 +2,7 @@ package org.jsmart.zerocode.core.httpclient.utils;
 
 import org.junit.Test;
 
-import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,41 +11,52 @@ import static org.hamcrest.core.Is.is;
 
 public class UrlQueryParamsUtilsTest {
 
+    public static final String BASE_URL = "http://localhost";
+
     @Test
-    public void testNotNull_queryParams() throws IOException {
+    public void testNotNull_queryParams() throws URISyntaxException {
         Map<String, Object> queryParamsMap = new HashMap<>();
         queryParamsMap.put("q1", "value1");
         queryParamsMap.put("q2", 3);
         queryParamsMap.put("q3", 1.9);
 
-        String qualifiedQueryParams = UrlQueryParamsUtils.createQualifiedQueryParams(queryParamsMap);
+        String qualifiedQueryParams = UrlQueryParamsUtils.setQueryParams(BASE_URL, queryParamsMap);
 
-        assertThat(qualifiedQueryParams, is("q1=value1&q2=3&q3=1.9"));
+        assertThat(qualifiedQueryParams, is(BASE_URL + "?q1=value1&q2=3&q3=1.9"));
     }
 
     @Test
-    public void testEmpty_queryParams() throws IOException {
+    public void testEmpty_queryParams() throws URISyntaxException {
         Map<String, Object> queryParamsMap = new HashMap<>();
-        String qualifiedQueryParams = UrlQueryParamsUtils.createQualifiedQueryParams(queryParamsMap);
-        assertThat(qualifiedQueryParams, is(""));
+        String qualifiedQueryParams = UrlQueryParamsUtils.setQueryParams(BASE_URL, queryParamsMap);
+        assertThat(qualifiedQueryParams, is(BASE_URL));
     }
 
     @Test
-    public void testNull_queryParams() throws IOException {
+    public void testNull_queryParams() throws URISyntaxException {
         Map<String, Object> queryParamsMap = null;
-        String qualifiedQueryParams = UrlQueryParamsUtils.createQualifiedQueryParams(queryParamsMap);
-        assertThat(qualifiedQueryParams, is(""));
+        String qualifiedQueryParams = UrlQueryParamsUtils.setQueryParams(BASE_URL, queryParamsMap);
+        assertThat(qualifiedQueryParams, is(BASE_URL));
     }
 
     @Test
-    public void testQueryParams_frontSlash() throws IOException {
+    public void testQueryParams_frontSlash() throws URISyntaxException {
         Map<String, Object> queryParamsMap = new HashMap<>();
         queryParamsMap.put("state/region", "singapore north");
         queryParamsMap.put("q2", "value2");
 
-        String qualifiedQueryParams = UrlQueryParamsUtils.createQualifiedQueryParams(queryParamsMap);
+        String qualifiedQueryParams = UrlQueryParamsUtils.setQueryParams(BASE_URL, queryParamsMap);
 
-        assertThat(qualifiedQueryParams, is("q2=value2&state%2Fregion=singapore+north"));
+        assertThat(qualifiedQueryParams, is(BASE_URL + "?q2=value2&state%2Fregion=singapore+north"));
     }
 
+    @Test
+    public void testQueryParamsCombinedWithUri() throws URISyntaxException {
+        Map<String, Object> queryParamsMap = new HashMap<>();
+        queryParamsMap.put("q2", "2");
+        String uriWithParams = BASE_URL + "?q1=1";
+
+        String qualifiedQueryParams = UrlQueryParamsUtils.setQueryParams(uriWithParams, queryParamsMap);
+        assertThat(qualifiedQueryParams, is(BASE_URL + "?q1=1&q2=2"));
+    }
 }
