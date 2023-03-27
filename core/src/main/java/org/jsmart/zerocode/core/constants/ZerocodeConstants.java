1e01e985ec756d90d09a52a42e4d9b53543ea98f
keerthi.narayan <keerthi.narayan@subex.com>
2023-03-27 12:34:34 +0530

    Changed the definition from interface to class

--- core/src/main/java/org/jsmart/zerocode/core/constants/ZerocodeConstants.java
+++ core/src/main/java/org/jsmart/zerocode/core/constants/ZerocodeConstants.java
@@ -2,14 +2,14 @@ package org.jsmart.zerocode.core.constants;
 
 import static org.jsmart.zerocode.core.utils.SmartUtils.readJsonAsString;
 
-public interface ZerocodeConstants {
-    String PROPERTY_KEY_HOST = "restful.application.endpoint.host";
-    String PROPERTY_KEY_PORT = "restful.application.endpoint.context";
+public class ZerocodeConstants {
+    public static final String PROPERTY_KEY_HOST = "restful.application.endpoint.host";
+    public static final String PROPERTY_KEY_PORT = "restful.application.endpoint.context";
 
-    String KAFKA = "kafka";
-    String KAFKA_TOPIC = "kafka-topic:";
-    String OK = "Ok";
-    String FAILED = "Failed";
+    public static final String KAFKA = "kafka";
+    public static final String KAFKA_TOPIC = "kafka-topic:";
+    public static final String OK = "Ok";
+    public static final String FAILED = "Failed";
 
-    String DSL_FORMAT = readJsonAsString("dsl_formats/dsl_parameterized_values.json");
+    public static final String DSL_FORMAT = readJsonAsString("dsl_formats/dsl_parameterized_values.json");
 }
