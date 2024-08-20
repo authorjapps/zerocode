12b862c7cf77123d15f75fe0ea40d5041bc8838c
prayascoriolis <prayas.kumar@coriolis.co.in>
2024-08-20 18:53:44 +0530

    test commit

--- http-testing/src/test/java/org/jsmart/zerocode/testhelp/tests/MetaDataTest/MetaDataTest.java
+++ http-testing/src/test/java/org/jsmart/zerocode/testhelp/tests/MetaDataTest/MetaDataTest.java
@@ -5,7 +5,7 @@ import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
+//
 @TargetEnv("github_host.properties")
 @RunWith(ZeroCodeUnitRunner.class)
 public class MetaDataTest {
