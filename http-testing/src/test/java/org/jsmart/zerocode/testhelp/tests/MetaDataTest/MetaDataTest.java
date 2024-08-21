0bb474422200673c60f3d86b933412f7cd2b1207
prayascoriolis <prayas.kumar@coriolis.co.in>
2024-08-21 14:55:49 +0530

    json file path corrected in metadatatest.java

--- http-testing/src/test/java/org/jsmart/zerocode/testhelp/tests/MetaDataTest/MetaDataTest.java
+++ http-testing/src/test/java/org/jsmart/zerocode/testhelp/tests/MetaDataTest/MetaDataTest.java
@@ -1,16 +1,16 @@
-package org.jsmart.zerocode.testhelp.tests.metadatatest;
+package org.jsmart.zerocode.testhelp.tests.MetaDataTest;
 
 import org.jsmart.zerocode.core.domain.JsonTestCase;
 import org.jsmart.zerocode.core.domain.TargetEnv;
 import org.jsmart.zerocode.core.runner.ZeroCodeUnitRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-//
+
 @TargetEnv("github_host.properties")
 @RunWith(ZeroCodeUnitRunner.class)
 public class MetaDataTest {
     @Test
-    @JsonTestCase("metadatatest/MetaDataTestAssertion.json")
+    @JsonTestCase("MetaDataTest/MetaDataTest.json")
     public void testmetadata() throws Exception {
 
     }
