f6feeaddd8e8e5e1f9d02bdcb0991a75b7d4675b
prayascoriolis <prayas.kumar@coriolis.co.in>
2024-09-26 10:09:55 +0530

    make folder and file names lowercase for consistency

--- http-testing/src/test/java/org/jsmart/zerocode/testhelp/tests/metadatatest/metadatatest.java
+++ http-testing/src/test/java/org/jsmart/zerocode/testhelp/tests/metadatatest/metadatatest.java
@@ -1,4 +1,4 @@
-package org.jsmart.zerocode.testhelp.tests.MetaDataTest;
+package org.jsmart.zerocode.testhelp.tests.metadatatest;
 
 import org.jsmart.zerocode.core.domain.JsonTestCase;
 import org.jsmart.zerocode.core.domain.TargetEnv;
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 
 @TargetEnv("github_host.properties")
 @RunWith(ZeroCodeUnitRunner.class)
-public class MetaDataTest {
+public class metadatatest {
     @Test
-    @JsonTestCase("MetaDataTest/MetaDataTest.json")
+    @JsonTestCase("metadatatest/metadatatest.json")
     public void testmetadata() throws Exception {
 
     }
