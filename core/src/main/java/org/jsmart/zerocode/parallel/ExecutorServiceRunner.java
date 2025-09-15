52fba3bd76eba1a7c529dda83e828ec0e10889ad
mykhailo-qdxp <mykhailo.tiutiun@proton.me>
2025-09-16 00:26:43 +0200

    ISSUE-730 # Fix: 'package.*' is in the imports

--- core/src/main/java/org/jsmart/zerocode/parallel/ExecutorServiceRunner.java
+++ core/src/main/java/org/jsmart/zerocode/parallel/ExecutorServiceRunner.java
@@ -8,7 +8,12 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.concurrent.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
